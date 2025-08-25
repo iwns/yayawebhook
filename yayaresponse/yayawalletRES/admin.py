a17a0df6589e79582e8128004f9cd23979e65044
iwns <96241541+iwns@users.noreply.github.com>
2025-08-25 15:27:56 +0300

    add model to admin display

--- yayaresponse/yayawalletRES/admin.py
+++ yayaresponse/yayawalletRES/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import paymentMethod
 
 # Register your models here.
+class paymentMet(admin.ModelAdmin):
+    list_display = ('payId','payerName','amount','date','cauuse','invoices_url')
+
+admin.site.register(paymentMethod,paymentMet)
